6324d798bb2571bd1136e6b43da75c809cfdcc80
EdwardDobson <47003895+EdwardDobson@users.noreply.github.com>
2019-02-09 14:15:36 +0000

    Started Layout of a dungeon

--- LuckTigerIsland/Assets/Scripts/Entities/PlayerChara.cs
+++ LuckTigerIsland/Assets/Scripts/Entities/PlayerChara.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerChara : CharaClass {
 
     public int mana;
     public int magicPower;
     public int EXP;
+    public Text healthText;
+    public Text strengthText;
+    public Text defenseText;
+    public Text speedText;
+    public Text levelText;
+    public Text manaText;
+    public Text magicPowerText;
+
 
     public PlayerChara(int hth, int str, int def, int spd, int lvl, int man, int mp, int exp)
     {
@@ -18,14 +26,21 @@ public class PlayerChara : CharaClass {
         mana = man;
         mp = magicPower;
         EXP = exp;
+       
     }
 
     public PlayerChara Warrior = new PlayerChara(100, 20, 20, 5, 1, 100, 10, 0);
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        healthText.text = " " + Warrior.health.ToString();
+        strengthText.text = " " + Warrior.strength.ToString();
+        defenseText.text = " " + Warrior.defense.ToString();
+        speedText.text = " " + Warrior.speed.ToString();
+        levelText.text = " " + Warrior.level.ToString();
+        manaText.text = " " + Warrior.mana.ToString();
+        magicPowerText.text = " " + Warrior.magicPower.ToString();
+    }
 	
 	// Update is called once per frame
 	void Update ()
--- LuckTigerIsland/Assets/Scripts/UI/FOR TESTING ONLY/Bar_Testing.cs
+++ LuckTigerIsland/Assets/Scripts/UI/FOR TESTING ONLY/Bar_Testing.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Bar_Testing : MonoBehaviour {
+    public PlayerChara player;
 
-    [SerializeField] private Health_Bar_Test m_healthBar;
+    [SerializeField] private Slider m_healthBar;
 	// Use this for initialization
 	private void Start () {
-		m_healthBar.SetBarSize(0.4f);
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        m_healthBar.value = player.GetHealth();
+        Debug.Log(m_healthBar.value);
+      
+    }
 }
