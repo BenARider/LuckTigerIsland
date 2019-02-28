8ab8b85e952caba1e3148db13f077c2a71fdf1f5
Ben Ritson <benjamin.ritson@gmail.com>
2019-02-28 19:25:10 +0000

    Basic Inventory
    Basic Inventory and equipment using scriptable objects

---
+++ LuckTigerIsland/Assets/Scripts/Inventory/InventoryObjects/Weapon.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "InventoryObject/Weapon")]
+public class Weapon : InventoryObject
+{
+    public int attack = 10;
+}
