f25954aaaa63d9514c4b5417514fdabe521d29aa
Ben Ritson <T7093422@live.tees.ac.uk>
2019-03-05 14:25:02 +0000

    Inventory Upgrades and Inventory Quest Object
    [description]

---
+++ LuckTigerIsland/Assets/Scripts/Inventory/InventoryPickup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryPickup : MonoBehaviour {
+
+	public InventoryObject iobject;
+
+    public void PickupObject() {
+        Inventory.Instance.AddToInventory(iobject);
+        EventManager.Instance.ItemToInventory(iobject);
+    }
+}
