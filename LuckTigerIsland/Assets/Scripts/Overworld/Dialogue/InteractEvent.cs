6e60cc2feab6b09bd3d8949e4fa116ba5d4ab364
BenARider <benarider@gmail.com>
2019-03-14 17:07:48 +0000

    fixed and merged

---
+++ LuckTigerIsland/Assets/Scripts/Overworld/Dialogue/InteractEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractEvent : MonoBehaviour
+{
+
+    public virtual void Interact(int argID)
+    {
+
+    }
+}
