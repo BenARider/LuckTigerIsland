b541688d9693326294a2c54d7e747361b75a37ff
Ben Ritson <benjamin.ritson@gmail.com>
2019-03-20 22:10:44 +0000

    Adding SHop back in
    SHOP HAS RETURNED

---
+++ LuckTigerIsland/Assets/Scripts/Inventory/Shop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct ShopItem
+{
+    public InventoryObject sItem;
+    public int sPrice;
+}
+
+public class Shop : MonoBehaviour
+{
+
+    [SerializeField]
+    public List<ShopItem> shop;
+
+    public void BuyItem(ShopItem _item)
+    {
+        if (Inventory.Instance.gold >= _item.sPrice)
+        {
+            Inventory.Instance.AddToInventory(_item.sItem);
+            Inventory.Instance.gold -= (_item.sPrice);
+        }
+        else
+        {
+            Debug.Log("Too expensive!");
+        }
+    }
+}
