4abd9fbd133ac9e96ab08374c5ce2042783cbd14
Ben Ritson <benjamin.ritson@gmail.com>
2019-03-28 16:59:59 +0000

    Shop Selling
    Shop Selling may or may not work. Before I was gonna test it, the PC
    turned off.

---
+++ LuckTigerIsland/Assets/Scripts/UI/ShopUI/SellListButton.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class SellListButton : MonoBehaviour, ISelectHandler
+{
+
+    [SerializeField]
+    private TextMeshProUGUI m_text;
+    [SerializeField]
+    private SellListControl m_sellControl;
+    Shop m_shop;
+    Inventory m_inventory;
+    InventoryObject m_inventoryObject;
+    private string m_name;
+    private Sprite m_image;
+    private string m_description;
+    private int m_price;
+    private int m_amount;
+    private int m_index;
+    private bool isNull = false;
+
+    public void SetText(string _text)
+    {
+        m_text.text = _text;
+        m_name = _text;
+    }
+    public void SetIndex(int _index)
+    {
+        m_index += _index;
+    }
+    public int GetIndex()
+    {
+        return m_index;
+    }
+    public void SetImage(Sprite _image)
+    {
+        m_image = _image;
+    }
+    public void SetDescription(string _desc)
+    {
+        m_description = _desc;
+    }
+    public void SetPrice(int _price)
+    {
+        m_price = _price;
+    }
+    public void SetAmount(int _amount)
+    {
+        m_amount = _amount;
+    }
+    public void SetObject(InventoryObject _object)
+    {
+        m_inventoryObject = _object;
+    }
+    public void SetIsNull(bool _b)
+    {
+        isNull = _b;
+    }
+    public void SellItem()
+    {
+        m_shop.SellItem(m_inventoryObject);
+        m_sellControl.UpdateInventoryUI();
+    }
+
+    public void OnSelect(BaseEventData _data)
+    {
+        if (!isNull)
+        {
+            m_sellControl.ButtonClicked(m_image, m_name, m_description, m_price);
+        }
+        else
+        {
+            Debug.LogError("Inventory Slot has nothing in it!");
+        }
+    }
+}
---
+++ LuckTigerIsland/Assets/Scripts/UI/ShopUI/SellListControl.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SellListControl : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject m_buttonTemplate;
+
+    [SerializeField]
+    private Image m_image;
+    [SerializeField]
+    private TextMeshProUGUI m_name;
+    [SerializeField]
+    private TextMeshProUGUI m_description;
+    [SerializeField]
+    private TextMeshProUGUI m_price;
+    int index = -1;
+
+
+    private void OnEnable()
+    {
+        UpdateInventoryUI();
+    }
+
+    public void UpdateInventoryUI()
+    {
+        try
+        {
+            if (Inventory.Instance.inventory.Count > 0)
+            {
+                ResetChildren();
+                SetDefaults();
+                for (int i = 0; i < Inventory.Instance.inventory.Count; i++)
+                {
+                    GameObject button = Instantiate(m_buttonTemplate) as GameObject;
+                    button.SetActive(true);
+
+                    if (Inventory.Instance.inventory[i].iObject != null)
+                    {
+                        button.GetComponent<SellListButton>().SetText(Inventory.Instance.inventory[i].iObject.name);
+                        button.GetComponent<SellListButton>().SetDescription(Inventory.Instance.inventory[i].iObject.Description);
+                        button.GetComponent<SellListButton>().SetImage(Inventory.Instance.inventory[i].iObject.Image);
+                        button.GetComponent<SellListButton>().SetPrice(Inventory.Instance.inventory[i].iObject.Price);
+                        button.GetComponent<SellListButton>().SetAmount(Inventory.Instance.inventory[i].amount);
+                        button.GetComponent<SellListButton>().SetObject(Inventory.Instance.inventory[i].iObject);
+                        button.GetComponent<SellListButton>().SetIndex(index);
+                    }
+                    else
+                    {
+                        button.GetComponent<SellListButton>().SetText("NullInvSlot");
+                        button.GetComponent<SellListButton>().SetIsNull(true);
+                    }
+
+                    button.transform.SetParent(m_buttonTemplate.transform.parent, false);
+
+                }
+            }
+        }
+        catch (System.NullReferenceException)
+        {
+            Debug.LogError("An Inventory does not exist in the scene! Is there a player?");
+        }
+    }
+
+    private void SetDefaults()
+    {
+        m_image.sprite = Inventory.Instance.inventory[0].iObject.Image;
+        m_name.text = Inventory.Instance.inventory[0].iObject.name;
+        m_description.text = Inventory.Instance.inventory[0].iObject.Description;
+        m_price.text = "Price: " + Inventory.Instance.inventory[0].iObject.Price;
+    }
+
+    private void ResetChildren()
+    {
+        bool first = false;
+        foreach (Transform child in m_buttonTemplate.transform.parent.transform)
+        {
+            if (!first)
+            {
+                first = true;
+            }
+            else
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+        }
+    }
+
+    public void ButtonClicked(Sprite _sprite, string _name, string _description, int _price)
+    {
+        m_image.sprite = _sprite;
+        m_name.text = _name;
+        m_description.text = _description;
+        m_price.text = "Price: " + _price.ToString();
+    }
+
+}
